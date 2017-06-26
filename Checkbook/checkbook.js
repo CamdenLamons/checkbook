2fbaf8b801a99d83b76553727639b7fa816e8245
CamdenLamons <camdenericlamons@icloud.com>
2017-06-26 15:09:15 -0400

    Added a pop up when click on the button.

--- Checkbook/checkbook.js
+++ Checkbook/checkbook.js
@@ -1,3 +1,9 @@
+// varablies
+
 function load(){
-    document.getElementById("total").innerHTML = "Total: $0.00"
+    document.getElementById("total").innerHTML = "Total: $0.00";
+    document.getElementById("popUp").style.display = 'none';
+}
+function add(){
+    document.getElementById("popUp").style.display = 'block';
 }
