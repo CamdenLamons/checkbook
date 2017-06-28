6d15842a8bc4a988df7806c1918a6adad82152e2
CamdenLamons <camdenericlamons@icloud.com>
2017-06-27 20:23:51 -0400

    Add a pop up for when the user does not enter the right info.

--- Checkbook/checkbook.js
+++ Checkbook/checkbook.js
@@ -4,14 +4,16 @@ var totalMoney = 0.00;
 // elements
 let totalElement = document.getElementById("total");
 let popUpElement = document.getElementById("popUp");
-let payeeElement = document.getElementById("Payee");
-let amountElement = document.getElementById("Amount")
+let payeeElement = document.getElementById("payee");
+let amountElement = document.getElementById("amount");
+let wrongAnswerElement = document.getElementById("wrongAnswer");
 
 // function is called when screen loads
 // function sets up everything up
 function load(){
     totalElement.innerHTML = "Total: $0.00";
     popUpElement.style.display = 'none';
+    wrongAnswerElement.style.display = 'none';
 }
 // function is called when clicked on the add button
 // function makes the pop up apper
@@ -25,12 +27,22 @@ function cancel(){
 }
 // function is called when click the submit button
 // function checks to make sure the user enter the right stuff then desplays how much money they have
+// function also display what when wrong if the user did not enter the write stuff
 function submit(){
     if ( payeeElement.value != "" && payeeElement.value != "Type Place Here" && Number(amountElement.value)){
         totalMoney += Number(amountElement.value);
         totalMoney_IndexOfDot = String(totalMoney).indexOf(".");
-        totalMoney_Slice = String(totalMoney).slice(0,totalMoney_IndexOfDot+3)
+        totalMoney_Slice = String(totalMoney).slice(0,totalMoney_IndexOfDot+3);
         totalElement.innerHTML = "Total: $" + String(totalMoney_Slice);
     }
+    else{
+        cancel();
+        wrongAnswerElement.style.display = 'block';
+    }
     cancel();
+}
+// function is called when the ok button is click
+// function hides the wrong answer pop up
+function ok(){
+    wrongAnswerElement.style.display = 'none';
 }
