a4f7d2d3d17c1fe94022d57f5a8c19fb89964cb4
CamdenLamons <camdenericlamons@icloud.com>
2017-06-30 15:39:17 -0400

    Cleaned up the code.

--- Checkbook/checkbook.js
+++ Checkbook/checkbook.js
@@ -5,6 +5,7 @@ if(!localStorage.savedMoney){
 }
 totalMoney = Number(localStorage.savedMoney);
 
+
 // elements
 let mainElement = document.getElementById("main");
 let totalElement = document.getElementById("total");
@@ -13,33 +14,57 @@ let payeeElement = document.getElementById("payee");
 let amountElement = document.getElementById("amount");
 let wrongAnswerElement = document.getElementById("wrongAnswer");
 
+
+// functions
+// order alphabetical
+
+// this function is called when clicked on the add transaction button
+// makes pop up apper
+function add(){
+    popUpElement.style.display = 'block';
+}
+
+// this function is called when the cancel button is click or at the end of submit
+// hides pop up
+function cancel(){
+    popUpElement.style.display = 'none';
+}
+
+// this function is called after the submit function and in the load function
+// adds the users inputs to the screen
+function info(){
+    var node = document.createElement("div");
+    var textnode = document.createTextNode("Info");
+    node.appendChild(textnode);
+    node.className = "mainInfo"
+    mainElement.appendChild(node);
+}
+
 // function is called when screen loads
-// function sets up everything up
+// sets everything up
 function load(){
     totalElement.innerHTML = "Total: $" + String(totalMoney);
     popUpElement.style.display = 'none';
     wrongAnswerElement.style.display = 'none';
 }
-// function is called when clicked on the erace button
-// function resets everything
+
+// this function is called when the ok button is click
+// hides wrong answer pop up
+function ok(){
+    wrongAnswerElement.style.display = 'none';
+}
+
+// this function is called when clicked on the erace button
+// resets everything
 function reset(){
     localStorage.savedMoney = 0.00;
     totalMoney = 0.00
     totalElement.innerHTML = "Total: $" + String(totalMoney)
 }
-// function is called when clicked on the add button
-// function makes the pop up apper
-function add(){
-    popUpElement.style.display = 'block';
-}
-// function is called when the cancel button is click or at the end of submit
-// function hides the pop up
-function cancel(){
-    popUpElement.style.display = 'none';
-}
-// function is called when click the submit button
-// function checks to make sure the user enter the right stuff then desplays how much money they have
-// function also display what when wrong if the user did not enter the write stuff
+
+// this function is called when click the submit button
+// checks to make sure the user enter the right stuff then desplays how much money they have
+// if did not enter right stuff display wrong answer
 function submit(){
     if ( payeeElement.value != "" && payeeElement.value != "Type Place Here" && Number(amountElement.value)){
         totalMoney += Number(amountElement.value);
@@ -54,18 +79,4 @@ function submit(){
         wrongAnswerElement.style.display = 'block';
     }
     cancel();
-}
-// function is called when the ok button is click
-// function hides the wrong answer pop up
-function ok(){
-    wrongAnswerElement.style.display = 'none';
-}
-// function is called after the submit function
-// fucntion adds the users input to the sreen
-function info(){
-    var node = document.createElement("div");
-    var textnode = document.createTextNode("Info");
-    node.appendChild(textnode);
-    node.className = "mainInfo"
-    mainElement.appendChild(node);
 }
